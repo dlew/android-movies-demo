4d048d35b48e4bc1da7469f9cd90b0a213027587
Daniel Lew <dan@mobiata.com>
2013-09-27 05:36:55 -0500

    Added basic POJO for movie data

---
+++ MoviesDemoProject/MoviesDemo/src/main/java/com/mobiata/moviesdemo/data/Movie.java
@@ -0,0 +1,13 @@
+package com.mobiata.moviesdemo.data;
+
+public class Movie {
+
+	private String mTitle;
+
+	private String mSynopsis;
+
+	private String mStoryline;
+
+	private String mPosterUrl;
+
+}
